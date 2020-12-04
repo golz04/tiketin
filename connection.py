15bfb7566d0d511fa372410cf8967dacff2556aa
ovisannn <ovi.sanjaya11@gmail.com>
2020-12-04 13:34:48 +0700

    menambahkan rasa yang pernah ada

---
+++ connection.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='tiketin'
+    )
+
+if db.is_connected():
+    print("databse is connected")
+else:
+    print("connection error")
